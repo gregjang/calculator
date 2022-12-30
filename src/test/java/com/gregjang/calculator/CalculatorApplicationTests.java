9cc7e070d467e06a353656ff2d71f067916c6cb9
osco <gregjang77@naver.com>
2022-12-30 13:51:00 +0900

    Add Spring Boot skeleton

---
+++ src/test/java/com/gregjang/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gregjang.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
